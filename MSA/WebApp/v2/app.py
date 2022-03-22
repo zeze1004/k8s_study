97d0ea6680036fd75f0cf6a5a1840aadf7acb28d
zeze1004 <thwjd2717@naver.com>
2022-03-23 03:02:08 +0900

    fix typo

--- MSA/WebApp/v2/app.py
+++ MSA/WebApp/v2/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     currentDT = datetime.datetime.now()
-    return "Welcome user! current time is " + str(currentDT)
+    return "ZEZE LOVE! current time is " + str(currentDT)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
